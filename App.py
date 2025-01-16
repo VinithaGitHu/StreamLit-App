ef69f4acafd58318df44195760aeb2b1ddea1715
VinithaGitHu <s.vinitha@dataentrega.com>
2025-01-16 19:21:25 +0530

    Update App.py

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ database_name = "DE_DWHM_DB"
 table_name = "[dbo].[SampleTable]"
 
 # Connection string
-connection_string = f"DRIVER={{ODBC Driver 17 for SQL Server}}; Server={server_name};DATABASE={database_name};Trusted_Connection=yes;TrustServerCertificate=yes;"
+connection_string = f"DRIVER={{ODBC Driver 17 for SQL Server}}; Server={server_name},5432;DATABASE={database_name};Trusted_Connection=yes;TrustServerCertificate=yes;"
 
 def fetch_data():
     """Fetch data from the SQL Server table."""
